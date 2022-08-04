2a87e77c90246a82bb1a776c8df2cfb3886459fe
modithapahan <modithapahan0@gmail.com>
2022-08-04 16:54:29 +0530

    Javascript file

---
+++ Variables/index.js
@@ -0,0 +1,25 @@
+var number = 12;
+var tree = "mango";
+var isTrue = tree;
+
+console.log(typeof(number));
+console.log(typeof(tree));
+console.log(typeof(isTrue));
+
+/* --------------------------------------- */
+
+var myName;
+console.log(myName);
+
+const ourNames = null;
+console.log(ourNames);
+
+/* --------------------------------------- */
+
+var myNumber = 4;
+var myString = "Variables are grate";
+var myBoolean = false;
+
+console.log("myNumber is equal to " + myNumber);
+console.log("myString is equal to " + myString);
+console.log("myBoolean is equal to " + myBoolean);
