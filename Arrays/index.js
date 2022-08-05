7023958929f9f94d2772069840d999a293491139
modithapahan <modithapahan0@gmail.com>
2022-08-05 22:15:57 +0530

    Arrays

---
+++ Arrays/index.js
@@ -0,0 +1,23 @@
+var myArray = [1,2,3];
+//or
+var theSameArray = new Array(1,2,3);
+
+//prinets the values.
+console.log(myArray[1]);
+console.log(theSameArray[0]);
+
+/* --------------------------------------------------------------------- */
+
+var myArray2 = [];
+myArray2[3] = "Hello";
+console.log(myArray2);
+
+/* Can have different type of values */
+var myArray3 = ["string", 10, {}]
+console.log(myArray3);
+
+/* -------------------------------------------------------------------- */
+var myArray4 = ["What is the meaning of life?", 22, true];
+console.log(myArray4[0]);
+console.log(myArray4[1]);
+console.log(myArray4[2]);
