141b057e0aed862e53c3a671259a7b8f4f48a39b
modithapahan <modithapahan0@gmail.com>
2022-08-10 15:12:11 +0530

    Array Manipulating

---
+++ Manipulating_Arrays/index.js
@@ -0,0 +1,12 @@
+/* push values to empty array */
+var myStack = [];
+
+myStack.push('My')
+myStack.push('Tree');
+myStack.push(3);
+
+console.log(myStack);
+
+/* pop values from the array */
+console.log(myStack.pop());
+console.log(myStack);
