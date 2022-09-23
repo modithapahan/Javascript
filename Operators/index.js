f62548e94b43fb5c56ccb11060a99e74d845cd72
modithapahan <modithapahan0@gmail.com>
2022-09-23 21:26:07 +0530

    Operators

---
+++ Operators/index.js
@@ -0,0 +1,15 @@
+var a = 1;
+var b = 2;
+var c = a + b;
+
+console.log(c);
+
+/* -------------------------------------------------------- */
+
+var name = "John";
+console.log("Hello " + name + "!");
+console.log("The meaning of life is " + 42);
+console.log(42 + " is the meaning of life");
+
+/* -------------------------------------------------------- */
+
